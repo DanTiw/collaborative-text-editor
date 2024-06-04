1c32d9a727022b9634acbeb78ce44c3f49fc379c
DanTiw <danishtiwari2204@gmail.com>
2024-06-04 22:33:25 +0530

    Added Real time with sockets

--- src/TextEditor.jsx
+++ src/TextEditor.jsx
@@ -1,9 +1,12 @@
-import React, { useState, useCallback } from "react"
+import React, {useState, useCallback, useEffect} from "react"
 import Quill from "quill"
 import "quill/dist/quill.snow.css"
-
+import { io } from "socket.io-client"
 export default function TextEditor() {
+    const [socket, setSocket] = useState();
     const [quill, setQuill] = useState()
+
+    //Add or Remove Toolbar options
     const TOOLBAR_OPTIONS = [
     [{ header: [1, 2, 3, 4, 5, 6, false] }],
     [{ font: [] }],
@@ -15,6 +18,42 @@ export default function TextEditor() {
     ["image", "blockquote", "code-block"],
     ["clean"],
 ]
+    //Set up socket
+    useEffect(() => {
+    const s = io("http://localhost:3001")
+    setSocket(s)
+        return () => {
+            s.disconnect()
+        }
+    }, []);
+    //Recieve changes
+    useEffect(() => {
+        if (socket == null || quill == null) return
+
+        const handler = delta => {
+            quill.updateContents(delta)
+        }
+        socket.on("receive-changes", handler)
+
+        return () => {
+            socket.off("receive-changes", handler)
+        }
+    }, [socket, quill])
+    //U
+       //Send changes
+    useEffect(() => {
+        if (socket == null || quill == null) return
+        const handler = (delta, oldDelta, source) => {
+            if (source !== "user") return
+            socket.emit("send-changes", delta)
+        }
+        quill.on("text-change", handler)
+        return () => {
+            quill.off("text-change", handler)
+        }
+    })
+    //Used callback to avoid infinite loop
+
     const wrapperRef = useCallback(wrapper => {
     if (wrapper == null) return
 
