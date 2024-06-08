b5a6f566c726916f70b917ecf51cda6c6ad7a447
DanTiw <danishtiwari2204@gmail.com>
2024-06-08 15:56:22 +0530

    finalized

--- client/src/TextEditor.js
+++ client/src/TextEditor.js
@@ -24,7 +24,7 @@ export default function TextEditor() {
 
   //Set up socket
   useEffect(() => {
-    const s = io("https://collaborative-text-editor-backend.vercel.app")
+    const s = io("https://collaborative-text-editor-backend-3fbhclwbs.vercel.app")
     setSocket(s)
     return () => {
       s.disconnect()
