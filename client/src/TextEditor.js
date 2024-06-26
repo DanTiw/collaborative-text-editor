e0d63cdff36a7366a0c91b9e970031e565d95f8b
Danish Tiwari <danishtiwari2204@gmail.com>
2024-06-26 12:39:27 +0530

    Update TextEditor.js

--- client/src/TextEditor.js
+++ client/src/TextEditor.js
@@ -24,7 +24,7 @@ export default function TextEditor() {
 
   //Set up socket
   useEffect(() => {
-  const s =  io("https://collaborative-text-editor-m8km.onrender.com")
+  const s =  io("http://localhost:3001")
     setSocket(s)
     return () => {
       s.disconnect()
